bede66c0ae26b26c4702cf5f54ff9fe1698c8e47
Zach Pratt <zppratt+github@gmail.com>
2016-01-29 14:36:53 -0500

    Ditching libgdx because single activity.  Need game to demonstrate various android concepts including activity, intent, processes, fragment, etc.

---
+++ app/src/main/java/com/prattlabs/adaringrescue/Actors/Player.java
@@ -0,0 +1,16 @@
+package com.prattlabs.adaringrescue.Actors;
+
+import com.prattlabs.adaringrescue.R;
+
+/**
+ * Created by zppratt on 1/28/16.
+ */
+public class Player {
+
+    PlayerView view;
+
+    public Player() {
+        view = (PlayerView) findViewById(R.drawable.player);
+    }
+
+}
---
+++ app/src/main/java/com/prattlabs/adaringrescue/Actors/PlayerView.java
@@ -0,0 +1,14 @@
+package com.prattlabs.adaringrescue.Actors;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by zppratt on 1/28/16.
+ */
+public class PlayerView extends ImageView {
+    public PlayerView(Context context) {
+        super(context);
+    }
+}
