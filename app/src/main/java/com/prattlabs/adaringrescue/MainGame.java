eabd60a1470973b6ec2007cda3a70dd55d467ec3
Zach Pratt <zppratt+github@gmail.com>
2016-01-30 16:45:13 -0500

    In the middle of debugging OOM errors after adding bitmaps

--- app/src/main/java/com/prattlabs/adaringrescue/MainGame.java
+++ app/src/main/java/com/prattlabs/adaringrescue/MainGame.java
@@ -1,5 +1,6 @@
 package com.prattlabs.adaringrescue;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.prattlabs.adaringrescue.drawing.GameBoard;
+
+import java.util.Random;
 
 /**
  * Created by zppratt on 1/29/16.
@@ -33,16 +36,40 @@ public class MainGame extends AppCompatActivity implements OnClickListener {
             public void run() {
                 initGfx();
             }
-        }, 1000); // Wait 1 second for the layout manager to run before initializing the graphics.
+        }, 5000); // Wait 1 second for the layout manager to run before initializing the graphics.
+    }
+
+    private Point getRandomPoint() {
+        System.err.println("getRandomPoint()");
+        Random r = new Random();
+        int minX = 0;
+        int maxX = getCanvas().getWidth() - getCanvas().getSprite1Width();
+        int x = 0;
+        int minY = 0;
+        int maxY = getCanvas().getHeight() - getCanvas().getSprite1Height();
+        int y = 0;
+        x = r.nextInt(maxX-minX+1)+minX;
+        y = r.nextInt(maxY-minY+1)+minY;
+        return new Point (x,y);
     }
 
     /**
      * Initialize the graphics.
      */
     synchronized public void initGfx() {
+        System.err.println("initGfx()");
         getCanvas().resetStarField();
+        //Select two random points for our initial sprite placement.
+        //The loop is just to make sure we don't accidentally pick
+        //two points that overlap.
+        Point p1, p2;
+        do {
+            p1 = getRandomPoint();
+            p2 = getRandomPoint();
+        } while (Math.abs(p1.x - p2.x) < getCanvas().getSprite1Width());
+        getCanvas().setSprite1(p1);
+        getCanvas().setSprite2(p2);
         getButton().setEnabled(true);
-        
         // Remove callbacks to keep from stacking up over time.
         frame.removeCallbacks(frameUpdate);
         frame.postDelayed(frameUpdate, FRAME_RATE);
@@ -68,14 +95,14 @@ public class MainGame extends AppCompatActivity implements OnClickListener {
      * Retrieves the game's button object for readability.
      */
     private Button getButton() {
-        return ((Button)findViewById(R.id.the_button));
+        return getButton();
     }
 
     /**
      * Retrieves the game board for readability.
      */
     private GameBoard getCanvas() {
-        return ((GameBoard)findViewById(R.id.the_canvas));
+        return getCanvas();
     }
 
 }
--- app/src/main/java/com/prattlabs/adaringrescue/drawing/GameBoard.java
+++ app/src/main/java/com/prattlabs/adaringrescue/drawing/GameBoard.java
@@ -1,11 +1,16 @@
 package com.prattlabs.adaringrescue.drawing;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.prattlabs.adaringrescue.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +29,41 @@ public class GameBoard extends View {
     private List<Point> starField = null;
     private int starAlpha = 80;
     private int starFade = 2;
+    private Rect sprite1Bounds = new Rect(0,0,0,0);
+    private Rect sprite2Bounds = new Rect(0,0,0,0);
+    private Point sprite1;
+    private Point sprite2;
+    private Bitmap bm1 = null;
+    private Bitmap bm2 = null;
     private static final int NUM_OF_STARS = 25;
+
+    synchronized public void setSprite1(Point p) {
+        sprite1=p;
+    }
+    synchronized public Point getSprite1() {
+        return sprite1;
+    }
+
+    synchronized public void setSprite2(Point p) {
+        sprite2=p;
+    }
+    synchronized public Point getSprite2() {
+        return sprite2;
+    }
+
+    synchronized public int getSprite1Width() {
+        return sprite1Bounds.width();
+    }
+    synchronized public int getSprite1Height() {
+        return sprite1Bounds.height();
+    }
+
+    synchronized public int getSprite2Width() {
+        return sprite2Bounds.width();
+    }
+    synchronized public int getSprite2Height() {
+        return sprite2Bounds.height();
+    }
 
     /**
      * Resets the starfield
@@ -41,6 +80,12 @@ public class GameBoard extends View {
     public GameBoard(Context context, AttributeSet aSet) {
         super(context, aSet);
         p = new Paint();
+        sprite1 = new Point(-1,-1);
+        sprite2 = new Point(-1,-1);
+        bm1 = BitmapFactory.decodeResource(getResources(), R.drawable.asteroid);
+        bm2 = BitmapFactory.decodeResource(getResources(), R.drawable.ufo);
+        sprite1Bounds = new Rect(0,0, bm1.getWidth(), bm1.getHeight());
+        sprite2Bounds = new Rect(0,0, bm2.getWidth(), bm2.getHeight());
     }
 
     /**
@@ -65,7 +110,11 @@ public class GameBoard extends View {
         p.setAlpha(255);
         p.setStrokeWidth(1);
         canvas.drawRect(0, 0, getWidth(), getHeight(), p);
+        drawStars(canvas);
+        drawSprites(canvas);
+    }
 
+    private void drawStars(Canvas canvas) {
         // Initialize the starfield, if needed, and draw the stars
         if (starField==null) {
             initializeStars(canvas.getWidth(),
@@ -82,4 +131,14 @@ public class GameBoard extends View {
             canvas.drawPoint(starField.get(i).x, starField.get(i).y, p);
         }
     }
+
+    private void drawSprites(Canvas canvas) {
+        if (sprite1.x>=0) {
+            canvas.drawBitmap(bm1, sprite1.x, sprite1.y, null);
+        }
+        if (sprite2.x>=0) {
+            canvas.drawBitmap(bm2, sprite2.x, sprite2.y, null);
+        }
+    }
+
 }
